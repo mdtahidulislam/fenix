7328cca509dc53c0bbe9364544a927305804cd06
mdtahidulislam <tahidul.developer@gmail.com>
2022-09-13 17:56:58 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# How To Use
+* install all package - yarn
+* create project into src folder e.g., all assets, html, css, imges and js
+* for devlopment: 
+    * "npm run dev or "yarn dev" - run server & create dist folder
+    * "npm run tailwind" or "yarn tailwind" - for tailwind css
+* for production: "npm run build" or "yarn build"
+* for making zip file: "npm run bundle" or "yarn bundle"
---
+++ src/assets/js/main.js
@@ -0,0 +1,103 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+      
+
+      // home slider 
+
+      const swiper = new Swiper(".home-slider", {
+        // effect: 'fade',
+        // fadeEffect: {
+        //   crossFade: true
+        // },
+        loop: true,
+        speed: 2500,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true,
+        },
+      });
+      $(".home-slider").mouseenter(function () {
+        swiper.autoplay.stop();
+      });
+      $(".home-slider").mouseleave(function () {
+        swiper.autoplay.start();
+      });
+      
+
+      // review slider
+      const reviewSwiper = new Swiper(".review-slide", {
+        // effect: 'fade',
+        // fadeEffect: {
+        //   crossFade: true
+        // },
+        loop: true,
+        speed: 1500,
+        autoplay: {
+          delay: 1500,
+          disableOnInteraction: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+      $(".review-slide").mouseenter(function () {
+        reviewSwiper.autoplay.stop();
+      });
+      $(".review-slide").mouseleave(function () {
+        reviewSwiper.autoplay.start();
+      });
+
+      // menu 
+      $('.js-menu-bar').on('click', function() {
+        $('.js-header-left').toggle();
+        $('.js-header-right').toggle();
+      });
+
+      
+      $('.js-mobile-menu-items li').each(function(idx, elm ) {
+        const delay = idx * 0.05;
+        $(this).css({'transitionDelay': `${delay}s`});
+      });
+
+      $('.js-menu-bar').on('click', function(){
+        $('.js-mobile-menu').addClass('open');
+      });
+      $('.js-mobile-menu-close').on('click', function(){
+        $('.js-mobile-menu').removeClass('open');
+      });
+
+      
+      // const tabelements = [
+      //   {
+      //     id: 'monthTab',
+      //     triggerEl: $('#month-tab'),
+      //     targetEl: $('#month')
+      //   },
+      //   {
+      //     id: 'yearTab',
+      //     triggerEl: $('#year-tab'),
+      //     targetEl: $('#year')
+      //   }
+      // ]
+      // const options = {
+      //   defaultTabId: 'yearTab',
+      //   activeClasses: 'text-red-600',
+      //   inactiveClasses: 'text-gray-500 ',
+      //   onShow: () => {
+      //     console.log('tab is shown');
+      //   }
+      // }
+      // const tabs = new Tabs(tabelements, options);
+      // tabs.show('yearTab');
+
+    });
+
+}(jQuery));
